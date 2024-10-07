e96401a158ec93e3aa770c1748a6a1b96c5f0a44
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-07 17:03:00 -0300

    add loops file

---
+++ Loops.java
@@ -0,0 +1,19 @@
+public class Loops {
+    public static void main(String[] args) {
+        for(var i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        int y = 0;
+        int x = 10;
+        while(y < x) {
+            System.out.println(y++);
+        }
+
+        int a = 0;
+        int b = 10;
+        do {
+            System.out.println(a++);
+        } while(a < b);
+    }
+}
