c199a44bed3bd13be5acda668aba26332ff44d2d
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-07 16:56:05 -0300

    add arrays file

---
+++ Arrays.java
@@ -0,0 +1,27 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] strArr = new String[2];
+        strArr[0] = "Word 01";
+        strArr[1] = "Word 02";
+
+
+        int[] intArr = new int[3];
+        intArr[0] = 47;
+        intArr[1] = 42;
+        intArr[2] = 77;
+
+        float[] fltArr = new float[4];
+        fltArr[0] = 1.84f;
+        fltArr[1] = 1.65f;
+        fltArr[2] = 1.55f;
+        fltArr[3] = 1.98f;
+
+        double[] dblArr = new double[5];
+        dblArr[0] = 55.26;
+        dblArr[1] = 46.25;
+        dblArr[2] = 89.25;
+        dblArr[3] = 22.25;
+        dblArr[4] = 79.22;
+
+    }
+}
