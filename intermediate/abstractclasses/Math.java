b1857a2c401b07b115707f3683d83b8b28a53c7e
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-09 11:59:54 -0300

    add comments

--- intermediate/abstractclasses/Math.java
+++ intermediate/abstractclasses/Math.java
@@ -3,5 +3,11 @@ package intermediate.abstractclasses;
 import java.util.ArrayList;
 
 public abstract class Math {
+    /* Abstract classes
+     * 1. Cannot create objects
+     * 2. Cannot add body to their methods
+     * 3. Can create regular methods
+     * 4. Can create non-value attributes*/
+
     public abstract double mathOp(ArrayList<Double> values);
 }
--- intermediate/interfaces/SuperSayiajinPowers.java
+++ intermediate/interfaces/SuperSayiajinPowers.java
@@ -1,6 +1,12 @@
 package intermediate.interfaces;
 
 public interface SuperSayiajinPowers {
+    /* Interfaces
+     * 1. Cannot create objects
+     * 2. Cannot add body to their methods
+     * 3. Every time you add an attribute it must have value, because its FINAL by default
+     * 4. Every single method is ABSTRACT by default, so when you implement an interface, its mandatory to initialize its methods*/
+
     void genkiDama();
     void kamehameha();
 }
